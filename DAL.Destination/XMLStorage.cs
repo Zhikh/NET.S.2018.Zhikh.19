37ed462d3d9483aba80cdf4cdd3cc4ce49b85c25
Domino <zhikhanastasya@gmail.com>
2018-08-01 23:16:26 +0300

    Add implementation

--- DAL.Destination/XMLStorage.cs
+++ DAL.Destination/XMLStorage.cs
@@ -2,14 +2,39 @@
 using DAL.Destination.Interface.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace DAL.Destination
 {
     public sealed class XMLStorage : IStorage<UrlAddress>
     {
+        private readonly FileInfo _file;
+
+        public XMLStorage(FileInfo file)
+        {
+            _file = file ?? throw new ArgumentNullException(nameof(file));
+        }
+
+        public XDocument GetContent()
+        {
+            return XDocument.Load(_file.Name);
+        }
+
         public void Save(IEnumerable<UrlAddress> entities)
         {
-            throw new NotImplementedException();
+            XmlSerializer serializer = new XmlSerializer(typeof(UrlAddresses));
+
+            UrlAddresses urlAddresses = new UrlAddresses
+            {
+                Addresses = entities
+            };
+
+            using (Stream stream = _file.OpenWrite())
+            {
+                serializer.Serialize(stream, urlAddresses);
+            }
         }
     }
 }
