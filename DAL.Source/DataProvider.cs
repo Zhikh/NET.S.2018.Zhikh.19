34f8d9539958748307a4de1b37e16f03d789b5a4
Domino <zhikhanastasya@gmail.com>
2018-08-01 23:33:08 +0300

    Add implementation

--- DAL.Source/DataProvider.cs
+++ DAL.Source/DataProvider.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using DAL.Source.Interface;
 
 namespace DAL.Source
 {
     public sealed class DataProvider : IDataProvider<string>
     {
+        private readonly FileInfo _file;
+
+        public DataProvider(FileInfo file)
+        {
+            _file = file ?? throw new ArgumentNullException(nameof(file));
+        }
+
         public IEnumerable<string> GetAll()
         {
-            throw new NotImplementedException();
+            using (StreamReader reader = new StreamReader(_file.FullName))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
         }
     }
 }
