bfd23f1771ce4cc741803a4ed88f3cc9bbc09738
Domino <zhikhanastasya@gmail.com>
2018-08-01 21:23:22 +0300

    Add files

---
+++ DependencyResolver/ResolverConfig.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Destination;
+using DAL.Destination.Interface;
+using DAL.Destination.Interface.Entities;
+using DAL.Source;
+using DAL.Source.Interface;
+using Ninject;
+
+namespace DependencyResolver
+{
+    public static class ResolverConfig
+    {
+        public static void ConfigurateResolver(this IKernel kernel)
+        {
+            kernel.Bind<IDataProvider<string>>().To<DataProvider>();
+            kernel.Bind<IStorage<UrlAddress>>().To<XMLStorage>();
+        }
+    }
+}
---
+++ UI.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyResolver;
+using BLL;
+using DAL.Source.Interface;
+using DAL.Destination.Interface;
+using DAL.Destination.Interface.Entities;
+
+namespace UI.Console
+{
+    class Program
+    {
+        private static readonly IKernel resolver;
+
+        static Program()
+        {
+            resolver = new StandardKernel();
+            resolver.ConfigurateResolver();
+        }
+
+        static void Main(string[] args)
+        {
+            var service = new DataTransferService(resolver.Get<IDataProvider<string>>(), 
+                resolver.Get<IStorage<UrlAddress>>());
+
+            service.Transfer();
+        }
+    }
+}
